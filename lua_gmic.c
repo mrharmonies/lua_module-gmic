1ae4ef87ea64769bdb5f4c364e9850e2a07e11d5
Mohd Kholid <mrharmonies@live.com>
2019-04-17 11:32:15 +0800

    Create lua_gmic.c

---
+++ lua_gmic.c
@@ -0,0 +1,32 @@
+#include "lualib.h"
+#include "lauxlib.h"
+#include "lua.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "gmic_libc.h"
+
+int gimik(lua_State *L) {
+  char *gcom=lua_tostring(L,-1);
+  gmic_interface_options options;
+  memset(&options,0,sizeof(gmic_interface_options));
+  options.ignore_stdlib = false;
+  bool abort = false;
+  float progress;
+  options.p_is_abort = &abort;
+  options.p_progress = &progress;
+  options.interleave_output = false;
+  options.no_inplace_processing = true;
+  options.output_format = E_FORMAT_FLOAT;
+  
+  gmic_call(gcom, 0, 0, &options);
+
+  return 0;
+}
+
+int luaopen_lua_gmic(lua_State *L){
+	lua_register(L,"gmic_call",gimik);
+	return 0;
+}
