e00b27355face60c57c2321a94f4ce0ef22d3d69
Mohd Kholid <mrharmonies@live.com>
2019-04-17 16:26:40 +0800

    Update lua_gmic.c

--- lua_gmic.c
+++ lua_gmic.c
@@ -12,17 +12,8 @@ int gimik(lua_State *L) {
   char *gcom=lua_tostring(L,-1);
   gmic_interface_options options;
   memset(&options,0,sizeof(gmic_interface_options));
-  options.ignore_stdlib = false;
-  bool abort = false;
-  float progress;
-  options.p_is_abort = &abort;
-  options.p_progress = &progress;
-  options.interleave_output = false;
-  options.no_inplace_processing = true;
-  options.output_format = E_FORMAT_FLOAT;
-  
+  options.ignore_stdlib = false; 
   gmic_call(gcom, 0, 0, &options);
-
   return 0;
 }
 
