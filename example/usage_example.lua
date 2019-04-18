9748f4394c85b4228228360080c8e8d2bd1131d0
Mohd Kholid <mrharmonies@live.com>
2019-04-18 10:03:32 +0800

    Update usage_example.lua

--- example/usage_example.lua
+++ example/usage_example.lua
@@ -1,1 +1,4 @@
 require ("lua_gmic")
+gmic("testimage2d 500 -o testimage.png") -- test image with dimension of 500x500 output to testimage.png
+gmic("solat.jpg +cartoon 3,80,15 -o solat2.jpg") -- cartoon effect. output to solat2.jpg
+gmic("lion.jpg +fire_edges , -o lion2.jpg") -- fire edges effect. output to lion2.jpg
